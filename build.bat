ac8788e3b2e1ac9db478f2430ee8524fef80d9a1
John R. Bradley <jrbradley@gmail.com>
2013-06-20 20:02:52 -0500

    Fixed the build and restructured the plugin a bit

--- build.bat
+++ build.bat
@@ -1,18 +1,36 @@
-
 pushd %_CWD% 
 set _CWD=%CD% 
 popd 
 
-for /f "delims=" %%a in ('"%GIT_BIN_DIR%\git" describe') do @set SPP_VERSION_BARE=%%a
+for /f "delims=" %%a in ('"%GIT_BIN_DIR%\git" describe') do @set CHP_VERSION_BARE=%%a
 
-set SPP_VERSION="SPP_VERSION=L"%SPP_VERSION_BARE%-x64""
+set CHP_VERSION="CHP_VERSION=L"%CHP_VERSION_BARE%-x64""
 msbuild /t:Rebuild /property:Configuration=Release;Platform=x64
 
-del SPP-%SPP_VERSION_BARE%.zip
-zip -j SPP-%SPP_VERSION_BARE%-x64.zip x64\Release\ServerPingPlugin.dll
+rd Releasex64 /s /q
+mkdir Releasex64
+mkdir Releasex64\CLRHostPlugin
 
-set SPP_VERSION="SPP_VERSION=L"%SPP_VERSION_BARE%-x86""
-msbuild /t:Rebuild /property:Configuration=Release;Platform=Win32
+copy CLRHostPlugin\x64\Release\CLRHostPlugin.dll Releasex64\
+copy CLRHostInterop\x64\Release\CLRHost.Interop.dll Releasex64\CLRHostPlugin\
 
-del SPP-%SPP_VERSION_BARE%.zip
-zip -j SPP-%SPP_VERSION_BARE%-x86.zip Release\ServerPingPlugin.dll
+
+cd Releasex64
+7z a CHP-%CHP_VERSION_BARE%-x64.7z .
+move CHP-%CHP_VERSION_BARE%-x64.7z ..
+cd ..
+
+set CHP_VERSION="CHP_VERSION=L"%CHP_VERSION_BARE%-x86""
+msbuild /t:Rebuild /property:Configuration=Release;Platform=win32
+
+rd Releasex86 /s /q
+mkdir Releasex86
+mkdir Releasex86\CLRHostPlugin
+
+copy CLRHostPlugin\win32\Release\CLRHostPlugin.dll Releasex86\
+copy CLRHostInterop\win32\Release\CLRHost.Interop.dll Releasex86\CLRHostPlugin\
+
+cd Releasex86
+7z a CHP-%CHP_VERSION_BARE%-x86.7z .
+move CHP-%CHP_VERSION_BARE%-x86.7z ..
+cd ..
